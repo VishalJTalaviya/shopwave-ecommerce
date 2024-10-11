e6f6a0a1e5af8a565b84bee3483c36b551e6bbed
VishalJTalaviya <144648163+VishalJTalaviya@users.noreply.github.com>
2024-10-11 10:09:17 +0530

    project done

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,16 +1,3 @@
-// import { configureStore } from '@reduxjs/toolkit' 
-// import cartSlice from './cartSlice'
-
-// export const store = configureStore({
-//   reducer: {
-//     cart : cartSlice
-//   },
-//   devTools : true
-// })
-
-
-
-
 
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './cartSlice';  // Import the default export
